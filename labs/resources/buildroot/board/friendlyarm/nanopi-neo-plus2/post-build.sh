17c362844ac0d02f5b786121a9ccb9e645a32398
Michael Mäder <michael.maeder@hefr.ch>
2025-11-21 13:25:57 +0100

    move to docker volume

--- labs/resources/buildroot/board/friendlyarm/nanopi-neo-plus2/post-build.sh
+++ labs/resources/buildroot/board/friendlyarm/nanopi-neo-plus2/post-build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 BOARD_DIR="$(dirname $0)"
-BUILDROOT_DIR="/workspace/buildroot"
+BUILDROOT_DIR="/buildroot/buildroot"
 
 #install -m 0644 -D $BOARD_DIR/extlinux.conf $BINARIES_DIR/extlinux/extlinux.conf
 mkimage -C none -A arm64 -T script -d $BOARD_DIR/boot.cmd $BUILDROOT_DIR/output/images/boot.scr
